d1755cece6c72395392332371b264969f8ad15dc
stasiaDiamond <nsisco@uw.edu>
2023-06-06 19:50:20 -0700

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+import Gallery from "./scenes/gallery";
+import ChildCare from "./scenes/childCarePage";
+import Cooking from "./scenes/cookingPage";
+import Cleaning from "./scenes/cleaningPage";
+import Dog from "./scenes/dogWalking";
+import Handjobs from "./scenes/handjobPage";
+import Yard from "./scenes/yardWorkPage";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
+import './index.css';
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+  const [show, setShow] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/childCarePage" element={<ChildCare />} />
+              <Route path="/cleaningPage" element={<Cleaning />} />
+              <Route path="/cookingPage" element={<Cooking />} />
+              <Route path="/dogWalking" element={<Dog />} />
+              <Route path="/handjobPage" element={<Handjobs />} />
+              <Route path="/yardWorkPage" element={<Yard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,221 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import "react-pro-sidebar/dist/css/styles.css";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import CollectionsIcon from '@mui/icons-material/Collections';
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import PetsIcon from '@mui/icons-material/Pets';
+import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import ChildFriendlyIcon from '@mui/icons-material/ChildFriendly';
+import HandymanIcon from '@mui/icons-material/Handyman';
+import YardIcon from '@mui/icons-material/Yard';
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <img
+                  alt="logo"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/logo-wht-bg.png`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Christian Gray
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Data
+            </Typography>
+            <Item
+              title="Clients Information"
+              to="/contacts"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Invoices Balances"
+              to="/invoices"
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="New Client"
+              to="/form"
+              icon={<PersonOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Pages
+            </Typography>
+        
+            <Item
+              title="Calendar"
+              to="/calendar"
+              icon={<CalendarTodayOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Gallery"
+              to="/gallery"
+              icon={<CollectionsIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Child Care"
+              to="/childCarePage"
+              icon={<ChildFriendlyIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Cleaning"
+              to="/cleaningPage"
+              icon={<CleaningServicesIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Cooking"
+              to="/cookingPage"
+              icon={<OutdoorGrillIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Dog Walking"
+              to="/dogWalking"
+              icon={<PetsIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Hand Jobs"
+              to="/handjobPage"
+              icon={<HandymanIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Yard Work"
+              to="/yardWorkPage"
+              icon={<YardIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+
+export default Sidebar;
