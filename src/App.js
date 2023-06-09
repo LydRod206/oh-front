2a646574de96bb45b878e839369cf50726d583da
stasiaDiamond <nsisco@uw.edu>
2023-06-09 15:53:42 -0700

    email2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
+import EmailTesting from "./components/SendEmailTesting"; // Added import
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import "./index.css";
+// import Login from './Login';
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  fetch("https://cryptic-woodland-28293.herokuapp.com/greeting").then(res =>
+  res.json()
+  ).then(data => console.log(data))
+
+
+  return (
+    <div className="App">
+      app
+
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/api/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/emailtesting" element={<EmailTesting />} />
+            </Routes>
+            </main>
+            </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
 // // App.js
 // import React, { useState } from "react";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -42,49 +89,49 @@
 
 
 
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Invoices from "./scenes/invoices";
-import Form from "./scenes/form";
-import EmailTesting from "./components/SendEmailTesting"; // Added import
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
-import "./index.css";
-// import Login from './Login';
+// import React, { useState } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Topbar from "./scenes/global/Topbar";
+// import Sidebar from "./scenes/global/Sidebar";
+// import Dashboard from "./scenes/dashboard";
+// import Invoices from "./scenes/invoices";
+// import Form from "./scenes/form";
+// import EmailTesting from "./components/SendEmailTesting"; // Added import
+// import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { ColorModeContext, useMode } from "./theme";
+// import "./index.css";
+// // import Login from './Login';
 
-function App() {
-  fetch("https://cryptic-woodland-28293.herokuapp.com/greeting").then(res =>
-  res.json()
-  ).then(data => console.log(data))
+// function App() {
+//   fetch("https://cryptic-woodland-28293.herokuapp.com/greeting").then(res =>
+//   res.json()
+//   ).then(data => console.log(data))
 
-  const [theme, colorMode] = useMode();
-  const [isSidebar, setIsSidebar] = useState(true);
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <Sidebar isSidebar={isSidebar} />
-          <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/api/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/emailtesting" element={<EmailTesting />} />
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-}
+//   return (
+//     // <div className="App">
+//     //   app
+//     // </div>
+//     <ColorModeContext.Provider value={colorMode}>
+//       <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <div className="app">
+//           <Sidebar isSidebar={isSidebar} />
+//           <main className="content">
+//             <Topbar setIsSidebar={setIsSidebar} />
+//             <Routes>
+//               <Route path="/" element={<Dashboard />} />
+//               <Route path="/api/invoices" element={<Invoices />} />
+//               <Route path="/form" element={<Form />} />
+//               <Route path="/emailtesting" element={<EmailTesting />} />
+//             </Routes>
+//             </main>
+//             </div></ThemeProvider>
+//     // </ColorModeContext.Provider>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
 
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -6,16 +6,9 @@ import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-// import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import CollectionsIcon from '@mui/icons-material/Collections';
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-// import PetsIcon from '@mui/icons-material/Pets';
-// import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
-// import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
-// import ChildFriendlyIcon from '@mui/icons-material/ChildFriendly';
-// import HandymanIcon from '@mui/icons-material/Handyman';
-// import YardIcon from '@mui/icons-material/Yard';
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
@@ -122,7 +115,7 @@ const Sidebar = () => {
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
             >
-              Data
+              DATA
             </Typography>
             <Item
               title="Invoices"
