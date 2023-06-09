df89105277d67525aed85089afbd709e85700680
Cyndi Fosmark <cmfosmark@gmail.com>
2023-06-09 09:34:09 -0700

    josh changes

--- src/api/API.js
+++ src/api/API.js
@@ -10,6 +10,24 @@ const API = {
             }
         })
     },
+    updateInvoice:(id, data) => {
+        return fetch(`${URL_PREFIX}/invoices/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error("failed to update invoice");
+            }
+        });
+    },
+    getAllJobs:() => {
+        
+    }
 };
 
 export default API;
