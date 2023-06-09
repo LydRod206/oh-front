5351a26973ebf01cd6a00f574a0338412e689523
stasiaDiamond <nsisco@uw.edu>
2023-06-09 15:57:10 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ function App() {
 
 
   return (
-    <div className="App">
-      app
-
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
@@ -34,7 +35,8 @@ function App() {
               <Route path="/emailtesting" element={<EmailTesting />} />
             </Routes>
             </main>
-            </div>
+            </div></ThemeProvider>
+          </ColorModeContext.Provider>
   );
 }
 
@@ -112,26 +114,7 @@ export default App;
 //     // <div className="App">
 //     //   app
 //     // </div>
-//     <ColorModeContext.Provider value={colorMode}>
-//       <ThemeProvider theme={theme}>
-//         <CssBaseline />
-//         <div className="app">
-//           <Sidebar isSidebar={isSidebar} />
-//           <main className="content">
-//             <Topbar setIsSidebar={setIsSidebar} />
-//             <Routes>
-//               <Route path="/" element={<Dashboard />} />
-//               <Route path="/api/invoices" element={<Invoices />} />
-//               <Route path="/form" element={<Form />} />
-//               <Route path="/emailtesting" element={<EmailTesting />} />
-//             </Routes>
-//             </main>
-//             </div></ThemeProvider>
-//     // </ColorModeContext.Provider>
-//   );
-// }
 
-// export default App;
 
 
 
