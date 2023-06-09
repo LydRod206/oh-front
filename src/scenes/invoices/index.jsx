7e7bd0a710cb4398ef633b1c2862e26090af5d71
Cyndi Fosmark <cmfosmark@gmail.com>
2023-06-09 13:42:36 -0700

    things

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,79 +1,160 @@
-import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+// import React, { useState, useEffect } from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGridPro, useGridApiContext } from '@mui/x-data-grid-pro';
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { useTheme } from "@mui/material";
 import API from "../../api/API";
+import { randomEmail } from '@mui/x-data-grid-generator';
+import { useForm, Controller } from 'react-hook-form';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
 
-const Contacts = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [invoices, setInvoices] = useState([]);
-  useEffect(() => {
-    API.getAllInvoices()
-      .then((data) => {
-        console.log(data.invoices);
-        setInvoices(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+function DetailPanelContent({ row }) {
+  const apiRef = useGridApiContext();
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({
+    defaultValues: row,
+    mode: 'onChange',
+  });
 
-  const columns = [
-    { 
-      field: "id", 
-      headerName: "ID", 
-      // flex: 0.5 
-    },
-    {
-      field: "date",
-      headerName: "Date",
-      flex: 0.3,
-    },
-    {
-      field: "client",
-      headerName: "Name",
-      flex: 0.3,
-      cellClassName: "name-column--cell",
-    },
-    {
-      field: "phone",
-      headerName: "Phone Number",
-      flex: 0.3,
-    },
-    {
-      field: "address",
-      headerName: "Address",
-      flex: 0.6,
-    },
-    {
-      field: "workDescription",
-      headerName: "Job Category",
-      flex: 0.3,
-    },
-    {
-      field: "cost",
-      headerName: "Cost",
-      flex: 0.2,
-    },
-    {
-      field: "expenses",
-      headerName: "Expenses",
-      flex: 0.2,
-    },
-    {
-      field: "isPaid",
-      headerName: "Paid",
-      flex: 0.2,
-    } 
-  ];
-  
+  const onSubmit = (data) => {
+    apiRef.current.updateRows([data]);
+    apiRef.current.toggleDetailPanel(row.id);
+  };
 
   return (
-    <Box m="20px">
+    <Stack
+      sx={{ py: 2, height: '100%', boxSizing: 'border-box' }}
+      direction="column"
+    >
+      <Paper sx={{ flex: 1, mx: 'auto', width: '90%', p: 1 }}>
+        <Stack
+          component="form"
+          justifyContent="space-between"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ height: 1 }}
+        >
+          <Typography variant="h6">{`Edit Order #${row.id}`}</Typography>
+          <Controller
+            control={control}
+            name="customer"
+            rules={{ required: true }}
+            render={({ field, fieldState: { invalid } }) => (
+              <TextField
+                label="Customer"
+                size="small"
+                error={invalid}
+                required
+                fullWidth
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: true }}
+            render={({ field, fieldState: { invalid } }) => (
+              <TextField
+                label="Email"
+                size="small"
+                error={invalid}
+                required
+                fullWidth
+                {...field}
+              />
+            )}
+          />
+          <div>
+            <Button
+              type="submit"
+              variant="outlined"
+              size="small"
+              disabled={!isValid}
+            >
+              Save
+            </Button>
+          </div>
+        </Stack>
+      </Paper>
+    </Stack>
+  );
+}
+
+
+// const Contacts = () => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   const [invoices, setInvoices] = useState([]);
+//   useEffect(() => {
+//     API.getAllInvoices()
+//       .then((data) => {
+//         console.log(data.invoices);
+//         setInvoices(data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    { field: "date", headerName: "Date", flex: 0.3 },
+    { field: "client", headerName: "Name", flex: 0.3 },
+    { field: "phone", headerName: "Phone Number", flex: 0.3 },
+    { field: "address", headerName: "Address", flex: 0.6 },
+    { field: "workDescription", headerName: "Job Category",flex: 0.3 },
+    { field: "cost", headerName: "Cost", flex: 0.2 },
+    { field: "expenses", headerName: "Expenses", flex: 0.2 },
+    { field: "isPaid", headerName: "Paid",flex: 0.2 } 
+  ];
+  const rows = [
+    {
+      id: 1,
+      customer: 'Matheus',
+      email: randomEmail(),
+    },
+    {
+      id: 2,
+      customer: 'Olivier',
+      email: randomEmail(),
+    },
+    {
+      id: 3,
+      customer: 'Flavien',
+      email: randomEmail(),
+    },
+    {
+      id: 4,
+      customer: 'Danail',
+      email: randomEmail(),
+    },
+    {
+      id: 5,
+      customer: 'Alexandre',
+      email: randomEmail(),
+    },
+  ];
+
+  export default function FormDetailPanel() {
+    const getDetailPanelContent = React.useCallback(
+      ({ row }) => <DetailPanelContent row={row} />,
+      [],
+    );
+
+    const getDetailPanelHeight = React.useCallback(() => 240, []);
+
+  return (
+    <Box sx={{ width: '100%', height: 400 }}>
       <Header
         title="CLIENTS"
         subtitle="List of Clients for Future Reference"
@@ -113,11 +194,12 @@ const Contacts = () => {
         <DataGrid
           rows={invoices}
           columns={columns}
-          slots={{ Toolbar: GridToolbar }}
+          rowThreshold={0}
+        getDetailPanelHeight={getDetailPanelHeight}
+        getDetailPanelContent={getDetailPanelContent}
         />
       </Box>
     </Box>
   );
 };
 
-export default Contacts;
